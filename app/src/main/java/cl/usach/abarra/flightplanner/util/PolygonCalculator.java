fcfa52a030758d213c661114311e480db7077171
aibarr <alfred.barra@usach.cl>
2017-08-30 12:00:26 -0300

    Calculo de distancia y creacion de utilidad de poligonos

---
+++ app/src/main/java/cl/usach/abarra/flightplanner/util/PolygonCalculator.java
@@ -0,0 +1,20 @@
+package cl.usach.abarra.flightplanner.util;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Created by Alfredo Barra on 30-08-2017. Pre-grade project.
+ */
+
+
+//TODO: añadir algoritmo para calcular poligonos
+public class PolygonCalculator {
+    List<LatLng> finalRoute;
+
+    public int calculatePolyroute(List<LatLng> vertices, float orientation){
+        return 0;
+    }
+}
+
